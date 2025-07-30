ed680b0631b11b9e0290e2a31fb026028a07e6b4
Sebastian Sokolowski <wielewiak08@gmail.com>
2025-07-30 11:24:18 +0200

    py-copy-file solution

--- app/main.py
+++ app/main.py
@@ -3,10 +3,8 @@ def copy_file(command: str) -> None:
         cmd, file_name, file_cp = command.split()
         if file_name != file_cp and cmd == "cp":
             try:
-                with open(file_name, "r") as file_in, open(file_cp, "w") as file_out:
+                with (open(file_name, "r") as file_in,
+                      open(file_cp, "w") as file_out):
                     file_out.write(file_in.read())
             except FileNotFoundError:
                 print(f"Error: {file_name} not found")
-
-
-
